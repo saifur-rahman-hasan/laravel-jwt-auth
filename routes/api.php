7aec0afb7cbb21b16a8cfea0b4bc81720c932bdc
Saifur Rahman <saifur.dohs@gmail.com>
2022-02-04 16:52:10 +0600

    me route removed and user route updated

--- routes/api.php
+++ routes/api.php
@@ -26,7 +26,6 @@ Route::group([
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
-    Route::post('me', [AuthController::class, 'me']);
     Route::get('user', [AuthController::class, 'user']);
 
 });
